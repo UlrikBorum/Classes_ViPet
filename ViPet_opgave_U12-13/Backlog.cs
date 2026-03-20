99b4efe49f8e3e4c313603ce2aa9fbee69df11f9
ThomasDalgaardHavaleska <thh009@edu.zealand.dk>
2026-03-20 11:47:48 +0100

    Add Backlog + Add-metode og remove-metode

--- ViPet_opgave_U12-13/Backlog.cs
+++ ViPet_opgave_U12-13/Backlog.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace ViPet_opgave_U12_13
 {
-    internal class Backlog
+    public class Backlog
     {
         #region instance fields 
 
-     
+        private List<Userstory> _userstories;
 
    
 
@@ -16,15 +16,53 @@ namespace ViPet_opgave_U12_13
 
         #region Constructor 
 
+        public Backlog()
+        {
+            _userstories = new List<Userstory>();
+
+        }
+
+        public Backlog(List<Userstory> userstories)
+        {
+            _userstories = userstories;
+        }
+
+
+
         #endregion
 
         #region properties 
 
+        public List<Userstory> Userstories
+        {
+            get { return _userstories; }
+            set { _userstories = value; }
+        }
 
         #endregion
 
         #region methods
 
+        public void AddUserstory(Userstory us)
+        { 
+            _userstories.Add(us);
+        }
+
+        public List<Userstory> RemoveUserstory(int id)
+        {
+            foreach (Userstory us in _userstories)
+            {
+                if (id == us.Id)
+                {
+                    _userstories.Remove(us);
+
+                    return _userstories;
+                }
+
+             }
+
+            return null;
+        }
 
         #endregion
     }
