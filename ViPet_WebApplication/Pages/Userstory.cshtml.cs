8612958abca7f7d3e5d871ad7efb50ba91d44120
Ulrik <ulb002@edu.zealand.dk>
2026-03-23 12:14:04 +0100

    Added pages: Backlog Sprint-backlog Userstory

---
+++ ViPet_WebApplication/Pages/Userstory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ViPet_WebApplication.Pages
+{
+    public class UserstoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ViPet_WebApplication/Pages/backlog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ViPet_WebApplication.Pages
+{
+    public class backlogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
