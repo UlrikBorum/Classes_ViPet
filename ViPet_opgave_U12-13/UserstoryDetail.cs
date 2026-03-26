1bdb7a153b3cc553cbd40a254d9f4deba5da9136
Christian Stenholm Wendt-Jensen <chw003@edu.zealand.dk>
2026-03-26 12:02:52 +0100

    added notes to every class in the library

--- ViPet_opgave_U12-13/UserstoryDetail.cs
+++ ViPet_opgave_U12-13/UserstoryDetail.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace ViPet_opgave_U12_13
 {
+	// We create a class called UserstoryDetail. This class has two instance fields, one for the description of the user story and one for a list of acceptance criteria. 
 	public class UserstoryDetail
 	{
 
@@ -16,12 +17,15 @@ namespace ViPet_opgave_U12_13
 
 		#region Constructor 
 
+		// We also have a default constructor that sets the description to an empty string and initializes the list of acceptance criteria to an empty list.
 		public UserstoryDetail()
 		{
 			_description = "";
 			_acList = new List<AcceptanceCriteria>();
 		}
 
+
+		// We also have a constructor that takes a string parameter for the description and a list of acceptance criteria as parameters and sets the instance fields to those parameters.
 		public UserstoryDetail(string description, List<AcceptanceCriteria> acList)
 		{
 			_description = description;
@@ -47,7 +51,9 @@ namespace ViPet_opgave_U12_13
 		#endregion
 
 		#region methods
-
+		
+		// The method for listing all the acceptance criteria returns a new list of acceptance criteria that is a copy of the original list, so that the original list cannot be modified from outside the class.
+		
 		public List<AcceptanceCriteria> ListAll()
 		{
 			return new List<AcceptanceCriteria>(_acList);
